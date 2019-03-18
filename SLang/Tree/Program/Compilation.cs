7acf29dd87d9d037dfbdbf8e715bff50627a8536
Denis Chernikov <tchernikoff.denis2013@yandex.ru>
2019-03-18 15:00:57 +0300

    Redundant COMPILATION element serialization excluded

--- SLang/Tree/Program/Compilation.cs
+++ SLang/Tree/Program/Compilation.cs
@@ -227,7 +227,7 @@ namespace SLang
         {
             return new JsonIr(GetType())
                 .AppendChild(JsonIr.ListToJSON(uses))
-                .AppendChild(JsonIr.ListToJSON(units_and_standalones))
+                //.AppendChild(JsonIr.ListToJSON(units_and_standalones))  // TODO: check
                 .AppendChild(ToJSON(anonymous));
         }
 
